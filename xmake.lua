f6862b8ebdd41371a47de8e1b36b81cbb0459da6
devalexxx <46360108+alspin8@users.noreply.github.com>
2024-09-30 23:13:15 +0200

    Add xmake.lua with Core module

---
+++ xmake.lua
@@ -0,0 +1,46 @@
+set_project("Hexis")
+set_version("0.0.1")
+
+add_rules("mode.debug", "mode.release")
+
+option("shared",   { description = "Build as shared library",     default = false })
+option("unittest", { description = "Build unittest (xmake test)", default = false })
+
+set_languages("c++23")
+
+if is_mode("debug") then
+	set_warnings("allextra", "error")
+end
+
+if has_config("unittest") then 
+	add_requires("doctest")
+end
+
+target("Core")
+	 add_defines("HEXIS_CORE_BUILD")
+
+    if has_config("shared") then
+        set_kind("shared")
+        add_defines("HEXIS_CORE_SHARED")
+    else
+        set_kind("static")
+    end
+
+    add_includedirs("include/", { public = true })
+	add_headerfiles("include/(Hexis/Core/*.h)", "include/(Hexis/Core/*.inl)")
+    --add_headerfiles("include/(Hexis/Core/**/*.h)")
+
+	add_files("src/Core/*.cpp")
+	-- add_files("src/Core/**/*.cpp")
+
+	if (has_config("unittest")) then 
+		for _, file in ipairs(os.files("test/Core/test_*.cpp")) do
+            add_tests(path.basename(file), {
+                kind = "binary",
+                files = file,
+                languages = "c++23",
+                packages = "doctest",
+                defines = "DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN"
+            })
+        end
+	end
