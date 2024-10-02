79ee78a3fc983072c619dddc9685d2faecfd052f
devalexxx <46360108+alspin8@users.noreply.github.com>
2024-10-02 22:43:42 +0200

    Add function to have prettier type name with gcc and clang

---
+++ include/Hexis/Core/TypeName.h
@@ -0,0 +1,20 @@
+//
+// Created by Alex on 01/10/2024.
+//
+
+#ifndef HEXIS_CORE_TYPENAME_H
+#define HEXIS_CORE_TYPENAME_H
+
+#include <string>
+
+namespace Hx
+{
+
+	template<typename T>
+	std::string TypeName();
+
+}
+
+#include <Hexis/Core/TypeName.inl>
+
+#endif
---
+++ src/Core/TypeName.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Alex Clorennec on 05/07/2025.
+//
+
+#include "Hexis/Core/TypeName.h"
+
+namespace Hx {}
